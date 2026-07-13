3a457fba69da65fdf2d869827810d0e4931d3428
Breno <147736256+BrenoNunes96@users.noreply.github.com>
2026-07-13 12:13:02 -0300

    Delete dist directory

--- dist/registroExecucoes/controllers/RE.controllers.d.ts
+++
@@ -1,7 +0,0 @@
-import { registroEntity } from "../entities/RE.entity";
-import { registroService } from "../Service/RE.service";
-export declare class registroController {
-    private readonly registroService;
-    constructor(registroService: registroService);
-    create(x: registroEntity): Promise<registroEntity | undefined>;
-}
--- dist/usuario/entities/usuario.entity.d.ts
+++
@@ -1,7 +0,0 @@
-import { AgenteEntity } from "../../Agente/Entities/agente.entity";
-export declare class usuarioEntity {
-    id: number;
-    usuario: string;
-    senha: string;
-    agente: AgenteEntity[];
-}
