27a420b94fab709753d248f1a6d788ce40f8512f
BrenoNunes96 <brenocp3@live.com>
2026-07-14 16:27:07 -0300

    fix

--- src/registroExecucoes/entities/RE.entity.ts
+++ src/registroExecucoes/entities/RE.entity.ts
@@ -11,23 +11,30 @@ export class registroEntity {
   @Column({ length: 255, nullable: false })
   @ApiProperty()
   mensagemDeEntrada!: string;
+  @ApiProperty()
 
   @Column({ length: 700, nullable: false })
+  @ApiProperty()
 
   mensagemDeSaida!: string;
+  @ApiProperty()
 
   @Column({ type: 'int', nullable: false })
   quantidadeDeTokensDeEntrada!: number;
+  @ApiProperty()
 
   @Column({ type: 'int', nullable: false })
   quantidadeDeTokensDeSaida!: number;
+  @ApiProperty()
 
   @Column({ type: 'int', nullable: false })
+  @ApiProperty()
 
   totaldeTokens!: number;
 
   @Column({ type: 'decimal', precision: 9, scale: 2, nullable: false })
-  
+    @ApiProperty()
+
   tempoDeExecucaoEmMilissegundos!: number;
 
   @ManyToOne(() => AgenteEntity, (x) => x['registroExecucao'], {
