5291d45ff66f63742bdb4b4ef49a55f51e4abcaf
BrenoNunes96 <brenocp3@live.com>
2026-08-07 16:45:05 -0300

    fix

--- dist/app.controller.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class AppController {
-    constructor();
-    redirect(resposta: any): Promise<any>;
-}
--- dist/usuario/entities/usuario.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { AgenteEntity } from '../../Agente/Entities/agente.entity';
-export declare class usuarioEntity {
-    id: number;
-    usuario: string;
-    senha: string;
-    agente: AgenteEntity[];
-    status: string;
-}
